68d4bc66f165b6f513e1665986ac287aa6c1dc10
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-29 21:48:21 -0700

    express的基本用法

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const express = require('express');
-console.log(express);
+// 1. 使用Express搭建Web服务器
+const express = require('express')
+
+const app = express()
+const port = 3030
+
+// 通过 get 请求访问路径
+app.get('/', (req, res) => {
+    res.send('Hi CLU 2025')
+})
+
+// 通过 post 请求登录
+app.post('/login', (req, res) => {
+    res.send('LOGIN SUCCESS')
+})
+
+// 在指定的端口启动服务器
+app.listen(port, () => {
+    console.log(`Server starts at port ${port} successfully!`)
+})
+
+// 2.使用nodemon自动重启服务器 npm install -g nodemon
