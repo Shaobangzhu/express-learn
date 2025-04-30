2a1fffda71973101da49e1bf0f6384a32b373348
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-29 22:10:21 -0700

    Request 对象介绍

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-// 1. 使用Express搭建Web服务器
+// 1. Request 对象的属性
+
+// 2. Request 对象的方法
+
 const express = require('express')
 
 const app = express()
 const port = 3030
 
-// 通过 get 请求访问路径
+// 通过 get 请求访问根路径
 app.get('/', (req, res) => {
-    res.send('Hi CLU 2025')
+    console.log(req.url)
+    console.log(req.path)
+    console.log(req.query)
+
+    res.send('Hi EXPRESS')
+})
+
+// 通过 get 请求访问/user
+// app.get('/user/:id', (req, res) => {
+app.get('/user', (req, res) => {
+    // console.log(req.url)
+    // console.log(req.path)
+    // console.log(req.query)
+
+    // console.log(req.method)
+
+    // console.log(req.headers)
+
+    // console.log(req.params)
+
+    // 方法
+    console.log(req.get('user-agent'));
+
+    res.send('Hi /user EXPRESS')
 })
 
 // 通过 post 请求登录
@@ -18,5 +44,3 @@ app.post('/login', (req, res) => {
 app.listen(port, () => {
     console.log(`Server starts at port ${port} successfully!`)
 })
-
-// 2.使用nodemon自动重启服务器 npm install -g nodemon
