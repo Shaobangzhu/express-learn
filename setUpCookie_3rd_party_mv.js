1a0dd2b59fd6e97a3fb5d9ba67aee5128badaa50
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-06 22:15:02 -0700

    set-up cookie

---
+++ setUpCookie_3rd_party_mv.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3030
+
+// 1. 在响应中设置cookie
+// Set-Cookie
+app.get('/cookie', (req, res)=> {
+    res.cookie('username', 'clu', {
+        maxAge: 30000
+    })
+    res.send('Hi Express')
+})
+
+// 2.清除Cookie
+app.get('/clearCookie', (req, res) => {
+    res.clearCookie('username')
+    res.send('Cookie Cleared')
+})
+
+// 3.打印Cookie
+app.get('/', (req, res) => {
+    console.log(req.get('cookie'))
+    res.send('打印Cookie')
+})
+
+app.listen(port, ()=>{
+    console.log(`服务器在${port}端口启动成功`)
+})
